cc598f9ec0ae85c05a1668c767f11eb09d7bab55
Shen-Teng Tu <tusamten@gmail.com>
2021-04-19 13:05:56 +0800

    Metadata and Docs URLs

--- web/main.py
+++ web/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from . import __version__
 
-app = FastAPI()
+app = FastAPI(
+    title="Simple Member System Implementation by FastAPI",
+    description="Simple Member System Implementation (source: https://github.com/ShenTengTu/py_sms_impl) .",
+    version=__version__,
+    docs_url="/api-doc",
+    redoc_url=None,
+)
 
 
 @app.get("/")
