8cc9247f160a1528c3c21f58d0e71eee1da4b60b
Shen-Teng Tu <tusamten@gmail.com>
2021-04-21 12:01:46 +0800

    switch translation by `Accept-Language`
    - Using `babel.core.Locale`
    - load translations on event `startup`
    - middleware: `parse_client_locale`
    - middleware: `update_translation`

--- web/main.py
+++ web/main.py
@@ -1,7 +1,13 @@
+from gettext import translation
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from . import __version__
-from .i18n import load_translations, get_translation
+from .i18n import (
+    load_translations,
+    get_translation,
+    is_babel_translation,
+    parse_accept_language,
+)
 from .tmpl import template_response, template_translation
 
 
@@ -13,10 +19,42 @@ app = FastAPI(
     redoc_url=None,
 )
 
-load_translations("en_US", "zh_TW", domain="py_sms_impl")
+
+@app.on_event("startup")
+async def startup_event():
+    load_translations("en_US", "zh_TW", domain="py_sms_impl")
+
+
+@app.middleware("http")
+async def observe_request_state(request: Request, call_next):
+    print(request.state._state)
+    response = await call_next(request)
+    return response
+
+
+@app.middleware("http")
+async def update_translation(request: Request, call_next):
+    trans = get_translation(None)  # null translation
+    for _, locale in request.state.accept_language:
+        trans = get_translation(locale)
+        if is_babel_translation(trans):
+            break
+    template_translation(trans)
+    response = await call_next(request)
+    return response
+
+
+@app.middleware("http")
+async def parse_client_locale(request: Request, call_next):
+    # get locale from HTTP header `Accept-Language`
+    request.state.accept_language = parse_accept_language(
+        request.headers["accept-language"]
+    )
+    response = await call_next(request)
+    return response
 
 
 @app.get("/")
 async def root(request: Request):
-    template_translation(get_translation("zh_TW"))
+    # template_translation(get_translation("zh_TW"))
     return template_response("index.html", {"request": request})
